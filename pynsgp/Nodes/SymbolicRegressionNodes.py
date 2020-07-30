2b46c81864df49648b6727e3579b9f79371796e3
marcovirgolin <marcovirgolin@gmail.com>
2020-07-30 16:31:33 +0200

    changed how sign is computed in DivNode

--- pynsgp/Nodes/SymbolicRegressionNodes.py
+++ pynsgp/Nodes/SymbolicRegressionNodes.py
@@ -65,7 +65,9 @@ class DivNode(Node):
 	def GetOutput( self, X ):
 		X0 = self._children[0].GetOutput( X )
 		X1 = self._children[1].GetOutput( X )
-		return np.multiply( np.sign(X1), X0) / ( 1e-6 + np.abs(X1) )
+		sign_X1 = np.sign(X1)
+		sign_X1[sign_X1==0]=1
+		return np.multiply( sign_X1, X0) / ( 1e-6 + np.abs(X1) )
 
 class AnalyticQuotientNode(Node):
 	def __init__(self):
